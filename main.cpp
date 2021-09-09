f32b323f962d12f455edc35d3943aa4e26a7f46d
Jonayed Mohiuddin <50599884+JonayedMohiuddin@users.noreply.github.com>
2021-09-09 15:42:50 +0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -14,14 +14,15 @@ FLOW CHART : Look at bottom.
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cmath>
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
 
-using std::to_string;  // Converts int to string
-using std::stod;       // Converst string to decimal
+using std::stod;      // Converst string to decimal
+using std::to_string; // Converts int to string
 
 /*#####################
 ##### TOKEN TYPES #####
@@ -29,25 +30,25 @@ using std::stod;       // Converst string to decimal
 
 enum TokenTypes
 {
-	UNKNOWN = -2,       // -2 UNKNOWN
-	_EOF = -1,          // -1 '\0'
-	OP_START = 0,       // Just to mark the start of operator enums in loops if needed
-	ADD,                //  1 '+'
-	SUBTRACT,           //  2 '-'
-	MULTIPLY,           //  3 '*'
-	DIVIDE,             //  4 '/'
-	EXPONENTIAL,        //  5 '^'
-	MODULO,             //  6 '%'
-	BRACKET_START,      //  7 '('
-	BRACKET_END,        //  8 ')'
-    LOG,                //  9 'log'
-    LN,                 // 10 'ln'
-    SIN,                // 11 'sin'
-    COS,                // 12 'cos'
-    TAN,                // 13 'tan'
-	OP_END,             // Just to mark the end of operator enums in loops if needed
-	NUMBER = 20,        // 20 '+'
-	MAXTOKEN_NUMBER
+    UNKNOWN = -2,  // -2 UNKNOWN
+    _EOF = -1,     // -1 '\0'
+    OP_START = 0,  // Just to mark the start of operator enums in loops if needed
+    PLUS,          //  1 '+'
+    MINUS,         //  2 '-'
+    MULTIPLY,      //  3 '*'
+    DIVIDE,        //  4 '/'
+    EXPONENTIAL,   //  5 '^'
+    MODULO,        //  6 '%'
+    BRACKET_START, //  7 '('
+    BRACKET_END,   //  8 ')'
+    LOG,           //  9 'log'
+    LN,            // 10 'ln'
+    SIN,           // 11 'sin'
+    COS,           // 12 'cos'
+    TAN,           // 13 'tan'
+    OP_END,        // Just to mark the end of operator enums in loops if needed
+    NUMBER = 20,   // 20 '+'
+    MAXTOKEN_NUMBER
 };
 
 /*###############
@@ -57,33 +58,73 @@ enum TokenTypes
 class Token
 {
 public:
-	TokenTypes type;
-	double value;
+    TokenTypes type;
+    double value;
 
-    Token() {};
+    Token(){};
 
-	Token(TokenTypes type)
-	{
-		this->type = type;
+    Token(TokenTypes type)
+    {
+        this->type = type;
         this->value = -1;
-	}
+    }
 
     Token(TokenTypes type, double value)
-	{
-		this->type = type;
-		this->value = value;
-	}
+    {
+        this->type = type;
+        this->value = value;
+    }
 
     int getTokenType(string &token)
     {
-        if(token == "log") return LOG;
-        else if(token == "ln")  return LN;
-        else if(token == "sin") return SIN;
-        else if(token == "cos") return COS;
-        else if(token == "tan") return TAN;
-        else                    return UNKNOWN;
+        if (token == "log")
+            return LOG;
+        else if (token == "ln")
+            return LN;
+        else if (token == "sin")
+            return SIN;
+        else if (token == "cos")
+            return COS;
+        else if (token == "tan")
+            return TAN;
+        else
+            return UNKNOWN;
     }
+
+    // $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+    // FOR DEBUG PURPOSE
+    string getTokenTypeString()
+    {
+        string tokenTypes[MAXTOKEN_NUMBER];
+        tokenTypes[PLUS] = "PLUS";
+        tokenTypes[MINUS] = "MINUS";
+        tokenTypes[MULTIPLY] = "MULTIPLY";
+        tokenTypes[DIVIDE] = "DIVIDE";
+        tokenTypes[EXPONENTIAL] = "EXPONENTIAL";
+        tokenTypes[MODULO] = "MODULO";
+        tokenTypes[BRACKET_START] = "BRACKET_START";
+        tokenTypes[BRACKET_END] = "BRACKET_END";
+        tokenTypes[LOG] = "LOG";
+        tokenTypes[LN] = "LN";
+        tokenTypes[SIN] = "SIN";
+        tokenTypes[COS] = "COS";
+        tokenTypes[TAN] = "TAN";
+        tokenTypes[NUMBER] = "NUMBER";
+
+        if(type == _EOF)
+            return "EOF";
+        else if(type == UNKNOWN)
+            return "UNKNOWN";
+        else
+            return tokenTypes[type];
+        
+    }
+    // $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 };
+
+/*###########################################
+##### TOKEN TYPES PRINTER DEBUG PURPOSE #####
+###########################################*/
 
 /*###############
 ##### LEXER #####
@@ -92,297 +133,376 @@ public:
 class Lexer
 {
 private:
-	string expression;
-	char curChar;
+    string expression;
+    char curChar;
 
-	int curPos;
-	int startPos;
+    int curPos;
+    int startPos;
 
 public:
-	Lexer()
-	{
-		this->expression = "";
-		curChar = ' ';
-		curPos = 0;
-	}
+    Lexer()
+    {
+        this->expression = "";
+        curChar = ' ';
+        curPos = 0;
+    }
 
-	Lexer(string &expression)
-	{
-		this->expression = expression;
-		curChar = ' ';
-		curPos = -1;
-		nextChar();
-	}
+    Lexer(string &expression)
+    {
+        this->expression = expression;
+        curChar = ' ';
+        curPos = -1;
+        nextChar();
+    }
 
 private:
-	void nextChar()
-	{
-		curPos++;
+    void nextChar()
+    {
+        curPos++;
 
-		if (curPos <= expression.length())
-			curChar = expression[curPos];
-		else
-			curChar = '\0';
-	}
+        if (curPos <= expression.length())
+            curChar = expression[curPos];
+        else
+            curChar = '\0';
+    }
 
-	char peekChar()
-	{
-		if ((curPos + 1) <= expression.length())
-			return expression[curPos + 1];
-		else
-			return '\0';
-	}
+    char peekChar()
+    {
+        if ((curPos + 1) <= expression.length())
+            return expression[curPos + 1];
+        else
+            return '\0';
+    }
 
-	void skipWhitespace()
-	{
-		while (curChar == ' ' || curChar == '\t' || curChar == '\r')
-		{
-			nextChar();
-		}
-	}
+    void skipWhitespace()
+    {
+        while (curChar == ' ' || curChar == '\t' || curChar == '\r')
+        {
+            nextChar();
+        }
+    }
 
-	void abort(string errMsg)
-	{
-		cout << errMsg;
-		exit(EXIT_FAILURE);
-	}
+    void abort(string errMsg)
+    {
+        cout << errMsg;
+        exit(EXIT_FAILURE);
+    }
 
 public:
-	Token getToken()
-	{
-		skipWhitespace();
+    Token getToken()
+    {
+        skipWhitespace();
 
-		Token token;
+        Token token;
 
-		if (curChar == '+')
-		{
-			token = Token(ADD, curChar);
-		}
+        if (curChar == '+')
+        {
+            token = Token(PLUS, curChar);
+        }
 
-		else if (curChar == '-')
-		{
-			token = Token(SUBTRACT, curChar);
-		}
+        else if (curChar == '-')
+        {
+            token = Token(MINUS, curChar);
+        }
 
-		else if (curChar == '*')
-		{
-			token = Token(MULTIPLY, curChar);
-		}
+        else if (curChar == '*')
+        {
+            token = Token(MULTIPLY, curChar);
+        }
 
-		else if (curChar == '/')
-		{
-			token = Token(DIVIDE, curChar);
-		}
+        else if (curChar == '/')
+        {
+            token = Token(DIVIDE, curChar);
+        }
 
-		else if (curChar == '^')
-		{
-			token = Token(EXPONENTIAL, curChar);
-		}
+        else if (curChar == '^')
+        {
+            token = Token(EXPONENTIAL, curChar);
+        }
 
-		else if (curChar == '%')
-		{
-			token = Token(MODULO, curChar);
-		}
+        else if (curChar == '%')
+        {
+            token = Token(MODULO, curChar);
+        }
 
-		else if (isdigit(curChar))
-		{
-			startPos = curPos;
+        else if (curChar == '(')
+        {
+            token = Token(BRACKET_START, curChar);
+        }
 
-			while (isdigit(peekChar()))
-				nextChar();
+        else if (curChar == ')')
+        {
+            token = Token(BRACKET_END, curChar);
+        }
 
-			if (peekChar() == '.')
-			{
-				nextChar();
-			}
+        else if (isdigit(curChar))
+        {
+            startPos = curPos;
 
-			while (isdigit(peekChar()))
-				nextChar();
+            while (isdigit(peekChar()))
+                nextChar();
 
-			double value = stod(expression.substr(startPos, curPos - startPos + 1)); // Get the number from the substring.
-			token = Token(NUMBER, value);
-		}
+            if (peekChar() == '.')
+            {
+                nextChar();
+            }
+
+            while (isdigit(peekChar()))
+                nextChar();
+
+            double value = stod(expression.substr(startPos, curPos - startPos + 1)); // Get the number from the substring.
+            token = Token(NUMBER, value);
+        }
 
         else if (isalpha(curChar))
-		{
-			startPos = curPos;
+        {
+            startPos = curPos;
 
             while (isalpha(peekChar()))
                 nextChar();
 
             string tokenText = expression.substr(startPos, curPos - startPos + 1);
-            TokenTypes tokenType = (TokenTypes) token.getTokenType(tokenText);
+            TokenTypes tokenType = (TokenTypes)token.getTokenType(tokenText);
 
             token = Token(tokenType);
-		}
+        }
 
-		else if (curChar == '\0')
-		{
-			token = Token(_EOF);
-		}
+        else if (curChar == '\0')
+        {
+            token = Token(_EOF);
+        }
 
-		else
-		{
-			token = Token(UNKNOWN);
-		}
+        else
+        {
+            token = Token(UNKNOWN);
+        }
 
-		nextChar();
-		return token;
-	}
+        nextChar();
+        return token;
+    }
 };
 
 /*###########################
 ##### CALCULATOR (MAIN) #####
 ###########################*/
-// Actually a parser.
 
 class Calculator
 {
-	string input;
-	Lexer lexer;
-	double result;
+    string input;
+    Lexer lexer;
+    double result;
 
-	Token curToken;
-	Token peekToken;
+    Token curToken;
+    Token peekToken;
 
 private:
-	void read()
-	{
-		cin >> input;
-	}
+    void read()
+    {
+        cin >> input;
+    }
 
-	void nextToken()
-	{
-		curToken = peekToken;
-		peekToken = lexer.getToken();
-	}
+    void nextToken()
+    {
+        curToken = peekToken;
+        peekToken = lexer.getToken();
+    }
 
-	bool isCurToken(TokenTypes type)
-	{
-		return (type == curToken.type) ? true : false;
-	}
+    bool isCurToken(TokenTypes type)
+    {
+        return (type == curToken.type) ? true : false;
+    }
 
-	bool isPeekToken(TokenTypes type)
-	{
-		return (type == peekToken.type) ? true : false;
-	}
+    bool isPeekToken(TokenTypes type)
+    {
+        return (type == peekToken.type) ? true : false;
+    }
 
-	void abort(string message)
-	{
-		cout << message << endl;
-		exit(EXIT_FAILURE);
-	}
+    void abort(string message)
+    {
+        cout << message << endl;
+        exit(EXIT_FAILURE);
+    }
 
-	// expression ::= term {( "-" | "+" ) term}
-	double expression()
-	{
-		cout << "~ > EXPRESSION (" << curToken.value << ")" << endl;
+    // expression ::= term {( "-" | "+" ) term}
+    double expression(int depth = 0)
+    {
+        cout << "~ >|\\    EXPRESSION (" << curToken.getTokenTypeString() << ")" << endl;
+        
+        double number = term();
 
-		double number = term();
+        // Can have 0 or more +/- and expressions.
+        while (isCurToken(PLUS) || isCurToken(MINUS))
+        {
+            if (isCurToken(PLUS))
+            {
+                nextToken();
+                number += term();
+            }
+            else if (isCurToken(MINUS))
+            {
+                nextToken();
+                number -= term();
+            }
+        }
 
-		// Can have 0 or more +/- and expressions.
-		while (isCurToken(ADD) || isCurToken(SUBTRACT))
-		{
-			if (isCurToken(ADD))
-			{
-				nextToken();
-				number += term();
-			}
-			else if (isCurToken(SUBTRACT))
-			{
-				nextToken();
-				number -= term();
-			}
-		}
+        return number;
+    }
 
-		return number;
-	}
+    // term ::= unary {( "*" | "/" | "%" | "^" | "") unary}
+    double term()
+    {
+        cout << "~ >| \\   TERM (" << curToken.getTokenTypeString() << ")" << endl;
 
-	// term ::= unary {( "*" | "/" ) unary}
-	double term()
-	{
-		cout << "~ > ^   TERM (" << curToken.value << ")" << endl;
+        double number = unary();
 
-		double number = unary();
+        // Can have 0 or more * or / and expressions.
+        while (isCurToken(MULTIPLY) || isCurToken(DIVIDE) || isCurToken(MODULO) || isCurToken(EXPONENTIAL) || isCurToken(BRACKET_START))
+        {
+            if (isCurToken(MULTIPLY))
+            {
+                nextToken();
+                number *= unary();
+            }
+            else if (isCurToken(DIVIDE))
+            {
+                nextToken();
+                number /= unary();
+            }
+            else if (isCurToken(MODULO))
+            {
+                nextToken();
+                number = (int)number % (int)unary();
+            }
+            else if (isCurToken(EXPONENTIAL))
+            {
+                nextToken();
+                number = pow(number, unary());
+            }
+            else if (isCurToken(BRACKET_START))
+            {
+                nextToken();
+                number *= unary();
+            }
+        }
 
-		// Can have 0 or more * or / and expressions.
-		while (isCurToken(MULTIPLY) || isCurToken(DIVIDE))
-		{
-			if (isCurToken(MULTIPLY))
-			{
-				nextToken();
-				number *= unary();
-			}
-			else if (isCurToken(DIVIDE))
-			{
-				nextToken();
-				number /= unary();
-			}
-		}
+        return number;
+    }
 
-		return number;
-	}
+    // unary ::= ["+" | "-"] primary
+    double unary()
+    {
+        cout << "~ >|  \\  UNARY (" << curToken.getTokenTypeString() << ")" << endl;
 
-	// unary ::= ["+" | "-"] number
-	double unary()
-	{
-		cout << "~ > ^^  UNARY (" << curToken.value << ")" << endl;
+        double number = 0;
+        short int sign = 1;
 
-		double number;
-		short int sign = 1;
+        while (isCurToken(PLUS) || isCurToken(MINUS))
+        {
+            if (isCurToken(PLUS))
+            {
+                nextToken(); // Skip the PLUS
+            }
+            else if (isCurToken(MINUS))
+            {
+                sign *= -1;
+                nextToken();
+            }
+        }
 
-		while (isCurToken(ADD) || isCurToken(SUBTRACT))
-		{
-			if (isCurToken(ADD))
-			{
-				nextToken(); // Skip the ADD
-			}
-			else if (isCurToken(SUBTRACT))
-			{
-				sign *= -1;
-				nextToken();
-			}
-		}
+        number = (double)sign * primary();
+        nextToken();
 
-		number = (double)sign * primary();
-		nextToken();
+        return number;
+    }
 
-		return number;
-	}
+    // primary ::= NUMBER | "("  expression  ")" | function ( NUMBER | "("  expression  ")" )
+    double primary()
+    {
+        cout << "~ >|___\\ PRIMARY (" << curToken.getTokenTypeString() << ")" << endl;
 
-	// number
-	double primary()
-	{
-		cout << "~ > ^^^ NUMBER (" << curToken.value << ")" << endl;
+        double number = 0;
 
-		if (!isCurToken(NUMBER))
-			abort("!! Expected a number");
+        if (isCurToken(NUMBER))
+        {
+            number = curToken.value;
+        }
+        else if(isCurToken(BRACKET_START))
+        {
+            nextToken();
+            number = expression(0);
+        }
+        else if(isCurToken(BRACKET_END))
+        {
+            nextToken();
+            return number;
+        }
+        else
+        {
+            number = prededefinedOperations();
+        }
 
-		return curToken.value;
-	}
+        return number;
+    }
+
+    double prededefinedOperations()
+    {
+        double number = 0;
+
+        if (isCurToken(LOG))
+        {
+            nextToken();
+            number = log10(primary());
+        }
+        else if(isCurToken(LN))
+        {
+            nextToken();
+            number = sin(primary());
+        }
+        else if(isCurToken(SIN))
+        {
+            nextToken();
+            number = sin(primary());
+        }
+        else if(isCurToken(COS))
+        {
+            nextToken();
+            number = sin(primary());
+        }
+        else if(isCurToken(TAN))
+        {
+            nextToken();
+            number = sin(primary());
+        }
+
+        return number;
+    }
 
 public:
-	double evaluate(string input = "")
-	{
-		if (input == "")
-			input = this->input;
+    double evaluate(string input = "")
+    {
+        if (input == "")
+            input = this->input;
 
-		cout << input << endl;
+        cout << "\nPlease ignore." << endl;
+        cout << "########### DEBUG TREE (START): ###########\n";
+        cout << "Input[In Calculator] = " << input << endl;
 
-		lexer = Lexer(input);
+        lexer = Lexer(input);
 
-		nextToken();
-		nextToken();
+        nextToken();
+        nextToken();
 
-		result = expression();
+        result = expression();
+
+        cout << "########### ::DEBUG TREE (END)  ###########\n\n";
 
         return result;
-	}
+    }
 
-	double answer()
-	{
-		return result;
-	}
+    double answer()
+    {
+        return result;
+    }
 };
 
 /*#######################
@@ -391,21 +511,18 @@ public:
 
 int main()
 {
-	cout << endl;
-	// string exp = "1000/4*-2+2*5*6-234+--5000";
-    string exp = "log ln sin cos tan";
-	Lexer lexer(exp);
-    cout << exp                        << endl;
-    cout << (int)lexer.getToken().type << endl;
-    cout << (int)lexer.getToken().type << endl;
-    cout << (int)lexer.getToken().type << endl;
-    cout << (int)lexer.getToken().type << endl;
-    cout << (int)lexer.getToken().type << endl;
-    
-    //Calculator calculator;
-	//cout << calculator.evaluate(exp) << endl;
+    string exp = "";
+    cout << "\nInput = ";
+    getline(cin, exp);
 
-	return 0;
+    Calculator calculator;
+    
+    double answer = calculator.evaluate(exp);
+
+    cout << "Input = " << exp << endl;
+    cout << "Ans = " << answer << endl;
+
+    return 0;
 }
 
 /*
@@ -423,8 +540,16 @@ FLOW CHART:
      \/                             |
 +------------+                      |
 | CALCULATOR |  >>--- NEXT TOKEN ---+ [calls]
-+------------+
++------------+   
      ||
 	 \/
+    AST TREE
+     ||
+	 \/
++------------+
+| CALCULATOR |  
++------------+   
+     ||
+     \/
 	RESULT
 */
